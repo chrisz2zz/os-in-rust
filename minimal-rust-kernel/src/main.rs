2c3ca222aff7452fd1bf7c36b2476bc6cec43230
ChrisZzz <dingyc2@chinatelecom.cn>
2023-03-09 19:07:42 +0800

    feat: 退出qemu

--- minimal-rust-kernel/src/main.rs
+++ minimal-rust-kernel/src/main.rs
@@ -1,5 +1,8 @@
 #![no_std] // 不链接 Rust 标准库
 #![no_main] // 不链接 Rust 标准库
+#![feature(custom_test_frameworks)]
+#![test_runner(crate::test_runner)]
+#![reexport_test_harness_main = "test_main"]
 
 use core::panic::PanicInfo;
 
@@ -20,6 +23,39 @@ pub extern "C" fn _start() -> ! {
     // 默认命名为 `_start`
 
     println!("Hello World{}", "!");
-    panic!("Some panic message");
+    // panic!("Some panic message");
+
+    #[cfg(test)]
+    test_main();
     loop {}
+}
+
+#[cfg(test)]
+fn test_runner(tests: &[&dyn Fn()]) {
+    println!("Running {} tests", tests.len());
+    for test in tests {
+        test();
+    }
+    exit_qemu(QemuExitCode::Success);
+}
+
+#[test_case]
+fn trivial_assertion() {
+    print!("trivial assertion... ");
+    assert_eq!(1, 1);
+    println!("[ok]");
+}
+
+pub enum QemuExitCode {
+    Success = 0x10,
+    Failed = 0x11,
+}
+
+pub fn exit_qemu(exit_code: QemuExitCode) {
+    use x86_64::instructions::port::Port;
+
+    unsafe {
+        let mut port = Port::new(0xf4);
+        port.write(exit_code as u32);
+    }
 }
