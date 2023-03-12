63bb0a7907578fa6134788c538f9d30230faa04a
ChrisZzz <2810744497@qq.com>
2023-03-12 17:25:03 +0800

    feat: test

--- minimal-rust-kernel/src/main.rs
+++ minimal-rust-kernel/src/main.rs
@@ -1,62 +1,32 @@
-#![no_std] // 不链接 Rust 标准库
-#![no_main] // 不链接 Rust 标准库
+#![no_std]
+#![no_main]
 #![feature(custom_test_frameworks)]
-#![test_runner(crate::test_runner)]
+#![test_runner(minimal_rust_kernel::test_runner)]
 #![reexport_test_harness_main = "test_main"]
 
 use core::panic::PanicInfo;
+use minimal_rust_kernel::println;
 
-mod vga_buffer;
-mod serial;
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    println!("Hello World{}", "!");
 
-/// 这个函数将在 panic 时被调用
-#[panic_handler] 
+    #[cfg(test)]
+    test_main();
+
+    loop {}
+}
+
+/// This function is called on panic.
+#[cfg(not(test))]
+#[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
     println!("{}", info);
     loop {}
 }
 
-static _HELLO: &[u8] = b"Hello World!";
-
-#[no_mangle] // 不重整函数名
-pub extern "C" fn _start() -> ! {
-    // 因为编译器会寻找一个名为 `_start` 的函数，所以这个函数就是入口点
-    // 默认命名为 `_start`
-
-    println!("Hello World{}", "!");
-    // panic!("Some panic message");
-
-    #[cfg(test)]
-    test_main();
-    loop {}
-}
-
 #[cfg(test)]
-fn test_runner(tests: &[&dyn Fn()]) {
-    println!("Running {} tests", tests.len());
-    for test in tests {
-        test();
-    }
-    exit_qemu(QemuExitCode::Success);
-}
-
-#[test_case]
-fn trivial_assertion() {
-    print!("trivial assertion... ");
-    assert_eq!(1, 1);
-    println!("[ok]");
-}
-
-pub enum QemuExitCode {
-    Success = 0x10,
-    Failed = 0x11,
-}
-
-pub fn exit_qemu(exit_code: QemuExitCode) {
-    use x86_64::instructions::port::Port;
-
-    unsafe {
-        let mut port = Port::new(0xf4);
-        port.write(exit_code as u32);
-    }
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    minimal_rust_kernel::test_panic_handler(info)
 }
